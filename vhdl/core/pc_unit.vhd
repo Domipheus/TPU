996f925280681ed1debd50536f99f290986e16c8
Colin Riley <colin@colsoft.biz>
2016-05-25 10:59:04 +0100

    Latest version (was missed earlier)

--- vhdl/core/pc_unit.vhd
+++ vhdl/core/pc_unit.vhd
@@ -18,12 +18,13 @@ entity pc_unit is
     Port ( I_clk : in  STD_LOGIC;
            I_nPC : in  STD_LOGIC_VECTOR (15 downto 0);
            I_nPCop : in  STD_LOGIC_VECTOR (1 downto 0);
+			  I_intVec: in STD_LOGIC;
            O_PC : out  STD_LOGIC_VECTOR (15 downto 0)
 			  );
 end pc_unit;
 
 architecture Behavioral of pc_unit is
-	signal current_pc: std_logic_vector( 15 downto 0) := X"0000";
+	signal current_pc: std_logic_vector( 15 downto 0) := ADDR_RESET;
 begin
 
 	process (I_clk)
@@ -31,12 +32,15 @@ begin
 		if rising_edge(I_clk) then
 			case I_nPCop is
 				when PCU_OP_NOP => 	-- NOP, keep PC the same/halt
+					if I_intVec = '1' then -- in a NOP, you can get intterupts. check.
+						current_pc <= ADDR_INTVEC;-- set PC to interrupt vector;
+					end if;
 				when PCU_OP_INC => 	-- increment
-					current_pc <= std_logic_vector(unsigned(current_pc) + 1);
+					current_pc <= std_logic_vector(unsigned(current_pc) + 2); -- BYTE ADDRESSING!
 				when PCU_OP_ASSIGN => 	-- set from external input
 					current_pc <= I_nPC;
 				when PCU_OP_RESET => 	-- Reset
-					current_pc <= X"0000";
+					current_pc <= ADDR_RESET;
 				when others =>
 			end case;
 		end if;
